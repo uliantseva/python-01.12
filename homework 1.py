c57aa57699df139c054ef5d813ad4f519e61ad70
Lexx1488 <svetaulianceva@gmail.com>
2022-12-03 20:36:12 +0200

    1.1

--- homework 1.py
+++ homework 1.py
@@ -1,3 +1,14 @@
-import antigravity
-print(antigravity)
-
+text = 'Single quotation mark\''
+print(text)
+text = 'Double quotation mark\"'
+print(text)
+text = 'New \nline'
+print(text)
+text = 'Backslash\\'
+print(text)
+text = 'Hori\tzontal ta\tb'
+print(text)
+text = 'Bell \aalert\a'
+print(text)
+text = 'Backsp\bace'
+print(text)
