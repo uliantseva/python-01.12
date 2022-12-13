5fe34da2e9def5d5ebac802b6260febb99ce39a8
Lexx1488 <svetaulianceva@gmail.com>
2022-12-13 12:11:29 +0200

    imput

--- homwork 2.py
+++ homwork 2.py
@@ -11,6 +11,20 @@ print(f'количество парт для всех учеников {(class1+
 
 
 
+num1 = int(input('Введите целое число:'))
+num2 = 0
+while num1 > 0:
+  digit = num1 % 10
+  num1 = num1 // 10
+  num2 = num2 * 10
+  num2 = num2 + digit
+print('"Обратное" ему число:', num2)
+
+
+
+
+
+
 
 
 
