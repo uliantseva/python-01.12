9368f76882787b1e68c74a2ac13bd220e840212c
Lexx1488 <svetaulianceva@gmail.com>
2022-12-17 20:18:28 +0200

    домашнее задание 2/ задача 2

---
+++ домашнее задание 2 задача 2.py
@@ -0,0 +1,4 @@
+class1 = int(input('количество учеников:'))
+class2 = int(input('количество учеников:'))
+class3 = int(input('количество учеников:'))
+print(f'количество парт для всех учеников {class1 // 2 + class1 % 2 + class2 // 2 + class2 % 2 + class2 // 2 + class2 % 2}')
