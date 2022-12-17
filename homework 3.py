2202d1a9f809b07786f2e39cbb433702cffb4bda
Lexx1488 <svetaulianceva@gmail.com>
2022-12-17 22:31:27 +0200

    домашнее задание 3

--- homework 3.py
+++ homework 3.py
@@ -1,11 +1,11 @@
 variable = input('Ввести знак операции:')
 n1 = float(input('Введите число:'))
 n2 = float(input('Введите число:'))
-if variable=='+':
+if variable == '+':
     print(n1+n2)
-elif variable=='-':
+elif variable == '-':
     print(n1 - n2)
-elif variable=='*':
+elif variable == '*':
     print(n1 * n2)
 elif variable == '/':
     print(n1 / n2)
@@ -14,6 +14,9 @@ elif variable == '**':
 else:
     print('Не указала знак операции')
     print('Не указала число операции')
+
+...
+
 
 
 
