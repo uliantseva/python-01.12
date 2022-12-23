4004d9f194a730872c014d2059761c77faa27f3e
Lexx1488 <svetaulianceva@gmail.com>
2022-12-23 23:07:24 +0200

    треугольники звездочкой

--- homework 4.py
+++ homework 4.py
@@ -43,7 +43,27 @@ for s in range(1, strings + 1):
     print()
 
 #
-
+strings = 7
+for s in range(1, strings + 1):
+    for space in range(1, strings - s + 1):
+        print(end='  ')
+    for star in range(1, 2 * s):
+        if star == 1 or star == s * 2 - 1:
+            print('*', end=' ')
+        else:
+            print('*', end=' ')
+    print()
+for i in range(strings - 1, 0, -1):
+    for j in range(1, strings - i + 1):
+        print(' ', end=' ')
+    for star in range(1, 2 * i):
+        if star == 1 or star == i * 2 - 1:
+            print('*', end=' ')
+        else:
+            print(' ', end=' ')
+    for k in range(1, strings - 6, 4):
+        print('-', end=' ')
+    print()
 
 
 
