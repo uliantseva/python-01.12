f56b2a2be3d972ab0333a572f4551a19a4795d12
Lexx1488 <svetaulianceva@gmail.com>
2022-12-24 19:38:43 +0200

    числа

--- домашка черновик.py
+++ домашка черновик.py
@@ -1,12 +1,9 @@
-simple_number = int(input('Введите число: '))
-d = 0
-for i in range(2, simple_number // 2+1):
-    if simple_number % i == 0:
-        d += d + 1
-if d <= 0:
-    print('Простое число')
-else:
-    print('Сложное число')
+my_string = '0123456789'
+for desytki in my_string:
+    for edinitsi in my_string:
+        result = int(desytki+edinitsi)
+        print(result, end=' ')
+
 
 
 
