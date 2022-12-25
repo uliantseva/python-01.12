9b432099ded8ba1b96a035a5b89d0e164b43d8e9
Lexx1488 <svetaulianceva@gmail.com>
2022-12-25 23:43:40 +0200

    списки

--- homwork 5.py
+++ homwork 5.py
@@ -1,2 +1,15 @@
 import random
-li = [random.randint(1, 1000) for i in range(50)]
+li = [random.randint(1, 1000) for i in range(15)]
+for i in li:
+    if i > 100:
+        print(i, end=':')
+
+###
+li = [random.randint(1, 1000) for i in range(15)]
+my_result = 0
+for i in range(100):
+    if my_result > 100:
+        print(i, end=' ')
+    else:
+        print(' ', end=' ')
+
