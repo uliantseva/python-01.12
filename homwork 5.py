f0fe640dcc857504f91dedc66f40dd01852f1868
Lexx1488 <svetaulianceva@gmail.com>
2022-12-26 22:35:15 +0200

    my_list

--- homwork 5.py
+++ homwork 5.py
@@ -1,7 +1,11 @@
 from random import randint
-li = [randint(100, 1000) for i in range(20)]
-print(li)
+my_list = [randint(100, 1000) for i in range(20)]
+print('my_list', my_list)
 
 ###
+my_list = [randint(100, 500) for h in range(10)]
+my_results = my_list
+print('my_results', my_results)
+
 
 
