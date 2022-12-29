26b9b08a85a4cff5c4f9e23949456aecefffe106
Lexx1488 <svetaulianceva@gmail.com>
2022-12-29 22:46:38 +0200

    коллекции

---
+++ homework 6.py
@@ -0,0 +1,17 @@
+## 3
+my_list_1 = [10, 11, 12, 13, 14, 15]
+my_list_2 = [1, 2, 3, 4, 5]
+result = my_list_1[::2] + my_list_2[::2]
+print(result)
+
+
+## 11
+my_str = 'fghbkkkljjk'
+my_result = [i for i in my_str if my_str.count(i) == 1]
+print(my_result)
+
+
+
+
+
+
