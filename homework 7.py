fd509f2ea81325f30cf1ec0151c9634525e322e7
Lexx1488 <svetaulianceva@gmail.com>
2023-01-09 22:17:05 +0200

    21 задача

--- homework 7.py
+++ homework 7.py
@@ -1,3 +1,5 @@
+# 1
+## а), б)
 persons = [
     {'name': 'John', 'age': 15},
     {'name': 'Katya', 'age': 24},
@@ -5,10 +7,24 @@ persons = [
     {'name': 'Masha', 'age': 20}
 ]
 age_min = persons[0]['age']
+long_name = len(persons)
 for person in persons:
     if person['age'] < age_min:
         age_min = person['age']
-print(age_min)
+print('age_min', age_min)
+for name in persons:
+    if len(name['name']) > long_name:
+        long_name = len(name['name'])
+print('long_name', long_name)
+# с
+count = 0
+for age in persons:
+    sum_age = sum('age')
+    count += 1
+average = sum_age / count
+print('The average of all the persons is:', average)
+
+
 
 
 
