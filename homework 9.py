e2b21467a765beab483f6a1f1d9fd5824898e051
Lexx1488 <svetaulianceva@gmail.com>
2023-01-16 20:35:50 +0200

    1

---
+++ homework 9.py
@@ -0,0 +1,4 @@
+# London is the capital of Great Britain. It is situated on the Thames.
+# It is a large industrial and cultural centre.
+# It population is about 9 million people.
+# The Thames dividesLondon into two parts: the West End and the East End.
