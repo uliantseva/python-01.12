0d6f787c2589fb4d787092537d87c68aa0def8d2
Lexx1488 <svetaulianceva@gmail.com>
2023-01-18 23:07:57 +0200

    censor

--- homework 9.py
+++ homework 9.py
@@ -1,10 +1,23 @@
-def censor(filename:str):
+def censor(filename: str):
     with open(filename, 'rt') as file:
         data = file.read()
         return data
-count = 0
-text = censor('files/London.txt')
-for i in censor:
+li = ['London', 'is', 'city']
+text = censor("files/London.txt")
+for i, j in li:
+    text = text.replace(i, j)
+print(text)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
