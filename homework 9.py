7b60982127811d6cdb4fb6fd2234be1a7ed4f4c4
Lexx1488 <svetaulianceva@gmail.com>
2023-01-21 23:40:36 +0200

    censor

--- homework 9.py
+++ homework 9.py
@@ -19,18 +19,31 @@ def censor(filename: str, list_words):
 censor("files/London.txt", ['London', 'Parliament', 'city'])
 
 # 2
-def fun_words(filename: str):
+def fun_words_count(filename: str):
     """
-    filename: "files/London.txt"
+    fun_words_count
     :param filename:
     :return:
     """
     with open(filename, 'rt') as file:
         text = file.read()
-        vocabulary = file.read()
-        return vocabulary
-        result = len(text.split())
-        print(result)
+        print(text)
+    words_dict = dict()
+    text = ("files/London.txt")
+    text = text.replace("\n", " ")
+    text = text.replace(",", "").replace(".", "").replace(":", "").replace("'", "")
+    text = text.casefold()
+    text = text.lower()
+    words = text.split()
+    words.sort()
+    print(words)
+    print(f"Кол-во слов: {len(words)}")
+    for word in words:
+        if word in words_dict:
+            words_dict[word] = words_dict[word] + 1
+        else:
+            words_dict[word] = 1
+    return words_dict
 
 
 
