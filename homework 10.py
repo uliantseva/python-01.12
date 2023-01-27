cc896118757b2d2f65d7f6c0b1e84a6e4d97d1e3
Lexx1488 <svetaulianceva@gmail.com>
2023-01-27 21:14:13 +0200

    json, csv

--- homework 10.py
+++ homework 10.py
@@ -1,25 +1,55 @@
 import json
-def censor(filename: str, list_words):
+import csv
 
+def censor(filename: str, list_words):
     """
-    list_words = ['London', 'Parliament', 'city']
-    filename: "files/London.txt"
+    list_words = ['London', 'Parliament', 'capital']
+    filename:
     :param filename:
     :param list_words:
     :return:
     """
-
     with open(filename, 'rt') as file:
         text = file.read()
-
-        for word in list_words:
-            text = text.replace(word, '*' * len(word))
-
+    for word in list_words:
+        text = text.replace(word, '*' * len(word))
+    print(text)
     with open("files/London 3.txt", "wt") as file:
         file.write(text)
 
-    word_count = {}
+censor("files/London.txt", ['London', 'Parliament', 'capital'])
 
+def censor_json(filename: str):
+    """
+    filename:
+    :param filename
+    :return:
+    """
+    with open(filename, 'rt') as file:
+        text = file.read()
+    word_count = {}
+    for word in text.split():
+        word = word.lower().strip('\n').replace(",", "").replace(".", "").replace(":", "").replace("'", "").replace(
+                "\n", " ")
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    print(word_count)
+    with open("files/London.stat.json", 'w') as file:
+        json.dump(word_count, file)
+censor_json("files/London.txt")
+
+def censor_csv(filename: str):
+    """
+    filename:
+    :param filename
+    :return:
+    """
+    with open(filename, 'rt') as file:
+        text = file.read()
+    stat = {}
+    word_count = {}
     for word in text.split():
         word = word.lower().strip('\n').replace(",", "").replace(".", "").replace(":", "").replace("'", "").replace(
             "\n", " ")
@@ -27,10 +57,18 @@ def censor(filename: str, list_words):
             word_count[word] += 1
         else:
             word_count[word] = 1
-
     print(word_count)
-    json_string = json.dumps(word_count)
-    print(type(json_string), json_string)
 
-censor("files/London.txt", ['London', 'Parliament', 'city'])
+    for word in word_count:
+        stat[word] = text.count(word)
+
+    with open("files/London.stat.csv", 'wt') as file:
+        writer = csv.writer(file)
+        writer.writerow(word_count)
+        for word, count in stat.items():
+            writer.writerow([word, count])
+
+censor_csv("files/London.txt")
+
+
 
