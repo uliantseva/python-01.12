968119fe72154e344ce06748e04d9f722cfbb231
Lexx1488 <svetaulianceva@gmail.com>
2023-02-02 16:49:03 +0200

    ошибка TypeError

--- Exceptions - 1.py
+++ Exceptions - 1.py
@@ -1,6 +1,17 @@
 try:
     divide_values = [2, 0, None, "1", True, False, [], {}]
     values_to_devide = [10, "1", None, True, False, [], 0, {}]
-    print(values_to_devide / divide_values)
+    result = values_to_devide / divide_values
+    print(result)
 except TypeError:
     print('Нельзя комбинировать эти типы данных')
+
+
+
+
+
+
+
+
+
+
