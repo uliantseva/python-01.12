ea9e73ad5ab7537369e6a211b21a4f62cb7742df
Lexx1488 <svetaulianceva@gmail.com>
2023-02-06 16:08:19 +0200

    Employee

--- домашка черновик.py
+++ домашка черновик.py
@@ -1,3 +1,5 @@
+import json
+import csv
 class Employee:
     """Создаем класс Employee"""
 
@@ -11,13 +13,24 @@ class Employee:
         self.people_lang = people_lang
         self.coding_lang = coding_lang
 
-    def save_data_to_file(self, filename):
-        with open(filename, 'w') as file:
-            file.write(f"Name: {self.firstname} {self.lastname}\n")
-            file.write(f"Age: {self.age}\n")
-            file.write(f"Email: {self.email}\n")
-            file.write(f"Skills: {', '.join(self.skills)}\n")
-            file.write(f"Languages: {', '.join(self.people_lang)}, {', '.join(self.coding_lang)}\n")
+    def save_data_to_file(self):
+        data_dict = {}
+        data_dict['firstname'] = self.firstname
+        data_dict['lastname'] = self.lastname
+        data_dict['age'] = self.age
+        data_dict['email'] = self.email
+        data_dict['skills'] = self.skills
+        data_dict['people_lang'] = self.people_lang
+        data_dict['coding_lang'] = self.coding_lang
+        with open("files/Employee.json", 'w') as file:
+            json.dump(data_dict, file, indent=4)
+        with open("files/Employee.csv", 'w') as file:
+            csv_writer = csv.writer(file)
+            for key, value in data_dict.items():
+                csv_writer.writerow([key, value])
+        print(data_dict)
+        return data_dict
+
 
 obj = Employee('Ivasik', 'Telesik', 13, 'ivasik-telesik1732@izkurnanog.ua',
                ["ходить", "говорить", "кодить"],
@@ -29,5 +42,4 @@ print(obj.email)
 print(obj.skills)
 print(obj.people_lang)
 print(obj.coding_lang)
-obj.save_data_to_file("files/Employee.json")
-obj.save_data_to_file("files/Employee.csv")
+obj.save_data_to_file()
