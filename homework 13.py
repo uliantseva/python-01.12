28b9af2f5c927c4721255619fe01580418085f3a
Lexx1488 <svetaulianceva@gmail.com>
2023-02-13 22:27:40 +0200

    family

--- homework 13.py
+++ homework 13.py
@@ -45,6 +45,22 @@ class Person:
             info = f'{info}phone: {self.__phone}\n'
         return info
 
+obj1 = Person('Ала', 'Давиденко', 40)
+#obj1.phone = '0997524589'
+#obj1.email = 'fghjkk@gmail.com'
+print(obj1)
+obj2 = Person('Олег', 'Давиденко', 41)
+#obj2.email = 'dffgy@gmail.com'
+#obj2.phone = '0507891258'
+print(obj2)
+obj3 = Person('Антон', 'Давиденко', 12)
+#obj3.phone = '0502367895'
+print(obj3)
+obj4 = Person('Тамара', 'Давиденко', 63)
+#obj4.phone = '0912365874'
+print(obj4)
+
+
 class Family(Person):
     """
     Класс описывающий семью
@@ -52,7 +68,7 @@ class Family(Person):
     __mom = str()
     __dad = str()
     __child = str()
-    __grandmother = str()
+    __grandma = str()
     __address = str()
     @property
     def mom(self):
@@ -64,55 +80,57 @@ class Family(Person):
     def child(self):
         return self.__child
     @property
-    def grandmother(self):
-        return self.__grandmother
+    def grandma(self):
+        return self.__grandma
     @property
     def address(self):
         return self.__address
     @address.setter
     def address(self, address: str):
         self.__address = address
-    def __set_age(self, age: int):
-        if age >= 0:
-            self.__age = age
-        else:
-            self.__age = 0
-    def __init__(self, person: Person, address):
+    def __init__(self, person: Person,  address):
         super().__init__(person.firstname, person.lastname, person.age)
         self.__address = address
+        #super().__init__(person2.firstname, person2.lastname, person2.age)
+        #self.__address = address
+        #super().__init__(person4.firstname, person4.lastname, person4.age)
+        #self.__address = address
     def __str__(self):
-        info = f'Name: {self.firstname} Surname: {self.lastname} Age: {self.age} address: {self.address}'
+        info = f'Name: {self.firstname} Surname: {self.lastname} Age: {self.age} address: {self.address}\n'
         if self.email != '':
             info = f'{info}e-mail: {self.__email}\n'
-        if self.phone != '':
-            info = f'{info}phone: {self.__phone}\n'
         return info
-    def get_dict(self):
+    def create_dict(self):
         data_dict = {}
-        data_dict['mom'] = self.firstname, self.lastname, self.age, self.address
-        data_dict['dad'] = self.firstname, self.lastname, self.age, self.address
-        data_dict['child'] = self.firstname, self.lastname, self.age, self.address
-        data_dict['grandmother'] = self.firstname, self.lastname, self.age, self.address
-        #for key, value in data_dict.items():
-                #([key, value])
-        print(data_dict)
+        data_dict['mom','dad','child','grandma'] = self.firstname, self.lastname, self.age, self.address
+        #data_dict['mom'] = self.firstname, self.lastname, self.age, self.address
+        #data_dict['dad'] = self.firstname, self.lastname, self.age, self.address
+        #data_dict['child'] = self.firstname, self.lastname, self.age, self.address
+        #data_dict['grandma'] = self.firstname, self.lastname, self.age, self.address
+        #print(data_dict)
         return data_dict
 
-obj1 = Person('Ала', 'Давиденко', 40)
-obj1.phone = '0997524589'
-obj1.email = 'fghjkk@gmail.com'
-print(obj1)
-obj2 = Person('Олег', 'Давиденко', 41)
-obj2.email = 'dffgy@gmail.com'
-obj2.phone = '0507891258'
-print(obj2)
-obj3 = Person('Антон', 'Давиденко', 12)
-print(obj3)
-obj4 = Person('Тамара', 'Давиденко', 63)
-obj4.phone = '0912365874'
-print(obj4)
-family = Family(obj1, 'ул. Плехановская 90')
-print(family)
+obj1 = Family(obj1, 'Плехановская 90')
+#print(obj)
+obj2 = Family(obj2, 'Плехановская 90')
+#print(obj2)
+obj3 = Family(obj3, 'ул. Плехановская 90')
+#print(obj3)
+obj4 = Family(obj4, 'ул. Плехановская 90')
+#print(obj4)
+print(obj1, obj2, obj3, obj4)
+obj1.create_dict(), obj2.create_dict(), obj3.create_dict(), obj4.create_dict()
+print(obj1.create_dict(), obj2.create_dict(), obj3.create_dict(), obj4.create_dict())
+
+
+
+
+
+
+
+
+
+
 
 
 
