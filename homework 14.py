bfbafe79f07908aa5a0d3c64b2e143ecdc68e3ec
Lexx1488 <svetaulianceva@gmail.com>
2023-02-21 18:56:52 +0200

    класс transport

--- homework 14.py
+++ homework 14.py
@@ -28,21 +28,77 @@ obj = Transport('1Q2EA', 4, 1000, 150)
 print(obj)
 
 class Bike(Transport):
-    def __init__(self, brakes: str, gov_number, number_of_wheels, power, max_speed):
+    """
+    Описание подкласса
+    """
+    def __init__(self, brakes: str, gov_number: str, number_of_wheels: int, power: int, max_speed: int):
         self.__brakes = brakes
         super().__init__(gov_number, number_of_wheels, power, max_speed)
     @property
     def brakes(self):
         return self.__brakes
     def __str__(self):
-        f'государственный номер: {self.__gov_number}\nколичество колес: {self.__number_of_wheels}\n' \
-        f'мощность двигателя: {self.__power}\nмаксимальная скорость: {self.__max_speed}\n' \
-        f'тормоза: {self.__brakes}\n'
-bik = Bike('Drum, Rim, Disc', 'номер отсутсвует', 2, 400, 200)
-print(bik.brakes)
-print(bik.gov_number)
-class Car(Transport):
-    pass
-class Bus(Transport):
-    pass
+        return super().__str__() + f'тормоза: {self.__brakes}\n'
 
+bike = Bike('Drum, Rim, Disc', 'номер отсутсвует', 2, 400, 200)
+print(bike)
+class Car(Transport):
+    """
+    Описание подкласса
+    """
+    def __init__(self, engine_number: str, gov_number: str, number_of_wheels: int, power: int, max_speed: int):
+        self.__engine_number = engine_number
+        super().__init__(gov_number, number_of_wheels, power, max_speed)
+    @property
+    def engine_number(self):
+        return self.__engine_number
+    def __str__(self):
+        return super().__str__() + f'номер двигателя: {self.__engine_number}\n'
+
+car = Car('2K34HJU', '1Q2EA', 4, 1000, 150)
+print(car)
+
+class Bus(Transport):
+    """
+    Описание подкласса
+    """
+    def __init__(self, number_of_seats: int, gov_number: str, number_of_wheels: int, power: int, max_speed: int):
+        self.__number_of_seats = number_of_seats
+        super().__init__(gov_number, number_of_wheels, power, max_speed)
+    @property
+    def number_of_seats(self):
+        return self.__number_of_seats
+    def __str__(self):
+        return super().__str__() + f'количество мест: {self.__number_of_seats}\n'
+
+
+bus = Bus(55, 'номер отсутсвует', 4, 1000, 150)
+print(bus)
+class Motorbike(Bike):
+    """
+    Описание подкласса
+    """
+    def __init__(self, moto_mirrors: int, brakes: str, gov_number: str, number_of_wheels: int, power: int, max_speed: int):
+        self.__moto_mirrors = moto_mirrors
+        super().__init__(brakes, gov_number, number_of_wheels, power, max_speed)
+    @property
+    def moto_mirrors(self):
+        return self.__moto_mirrors
+    def __str__(self):
+        return super().__str__() + f'зеркала на мотоцикле: {self.__moto_mirrors}\n'
+moto = Motorbike(2, 'Drum, Disc', 'AAA56G', 2, 1000, 150)
+print(moto)
+class Truck(Car):
+    """
+     Описание подкласса
+    """
+    def __init__(self, type: str, engine_number: str, gov_number: str, number_of_wheels: int, power: int, max_speed: int):
+        self.__type = type
+        super().__init__(engine_number, gov_number, number_of_wheels, power, max_speed)
+    @property
+    def type(self):
+        return self.__type
+    def __str__(self):
+        return super().__str__() + f'тип грузовика: {self.__type}\n'
+truck = Truck('закрытый, тентованный', '2K34HJU', '1Q2EA', 4, 1000, 150)
+print(truck)
