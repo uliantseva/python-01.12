1c5b1c510807573b80d9486b56b0c0ee58de88a8
Lexx1488 <svetaulianceva@gmail.com>
2023-02-22 23:01:40 +0200

    класс животные

--- homework 14(1).py
+++ homework 14(1).py
@@ -15,61 +15,31 @@ class Animals:
     @property
     def mov_speed(self):
         return self.__mov_speed
+    def make_a_sound(self):
+        print("Издаёт животный звук")
     def __str__(self):
         return f'название: {self.__name}\nвес: {self.__weight}\nскорость передвижения: {self.__mov_speed}\n'
 obj = Animals('hummingbird', 5, 98)
 print(obj)
 
+
 class Birds(Animals):
     """
     Подкласс описывающий птиц
     """
-    def __init__(self, number_of_feathers: int, name: str, weight: int, mov_speed: int):
-        self.__number_of_feathers = number_of_feathers
+    def __init__(self,  body_length: int, name: str, weight: int, mov_speed: int):
+        self.__body_length = body_length
         super().__init__(name, weight, mov_speed)
     @property
     def number_of_feathers(self):
-        return self.__number_of_feathers
+        return self.__body_length
+    def make_a_sound(self):
+        print("Издаёт животный звук")
     def __str__(self):
-        return super().__str__() + f'количество перьев: {self.__number_of_feathers}\n'
-birds = Birds(5000,'hummingbird', 5, 98)
+        return super().__str__() + f'длина тела: {self.__body_length}\n'
+birds = Birds(10,'hummingbird', 5, 10)
 print(birds)
 
-class Paleognathae(Birds):
-    """
-    Подкласс описывающий бескилевых птиц
-    """
-    def __init__(self, inf_rank: str, number_of_feathers: int, name: str, weight: int, mov_speed: int):
-        self.__inf_rank = inf_rank
-        super().__init__(number_of_feathers, name, weight, mov_speed)
-    @property
-    def inf_rank(self):
-        return self.__inf_rank
-
-    def __str__(self):
-        return super().__str__() + f'ранг инфракласса: {self.__inf_rank}\n'
-pal= Paleognathae('casuariiformes', 5000, 'emu', 37, 50)
-print(pal)
-
-class Neognathae(Birds):
-    """
-    Подкласс описывающий новонёбных птиц
-    """
-    def __init__(self, families: str, inf_rank: str, number_of_feathers: int, name: str, weight: int, mov_speed: int):
-        self.__inf_rank = inf_rank
-        self.__families = families
-        super().__init__(number_of_feathers, name, weight, mov_speed)
-    @property
-    def inf_rank(self):
-        return self.__inf_rank
-    @property
-    def families(self):
-        return self.__families
-
-    def __str__(self):
-        return super().__str__() + f'ранг инфракласса: {self.__inf_rank}\nсемейства: {self.__families}\n'
-neo = Neognathae('phasianidae', 'galliformes', 6000,'peacock', 6, 50)
-print(neo)
 
 class Reptiles(Animals):
     """
@@ -85,6 +55,62 @@ class Reptiles(Animals):
         return super().__str__() + f'количество лап: {self.__number_of_paws}\n'
 reptiles = Reptiles(4, 'anura', 700, 4)
 print(reptiles)
+
+
+class Mammals(Animals):
+    """
+    Подкласс описывающий млекопитающих
+    """
+    def __init__(self, body_length: int, name: str, weight: int, mov_speed: int):
+        self.__body_length = body_length
+        super().__init__(name, weight, mov_speed)
+    @property
+    def body_length(self):
+        return self.__body_length
+    def __str__(self):
+        return super().__str__() + f'длинна тела: {self.__body_length}\n'
+mammals = Mammals(3,'kangu', 47, 70)
+print(mammals)
+
+
+class Stork(Birds):
+    """
+    Подкласс описывающий бескилевых птиц
+    """
+    def __init__(self, type: str, body_length: int, name: str, weight: int, mov_speed: int):
+        self.__type = type
+        super().__init__(body_length, name, weight, mov_speed)
+    @property
+    def inf_rank(self):
+        return self.__type
+    def make_a_sound(self):
+        print('АРРР')
+    def __str__(self):
+        return super().__str__() + f'тип: {self.__type}\n'
+stork = Stork('Horde', 110, 'stork', 3, 45)
+print(stork)
+stork.make_a_sound()
+
+class Ara(Birds):
+    """
+    Подкласс описывающий новонёбных птиц
+    """
+    def __init__(self, type: str, families: str,  body_length: int, name: str, weight: int, mov_speed: int):
+        self.__type = type
+        self.__families = families
+        super().__init__(body_length, name, weight, mov_speed)
+    @property
+    def inf_rank(self):
+        return self.__type
+    @property
+    def families(self):
+        return self.__families
+    def __str__(self):
+        return super().__str__() + f'тип: {self.__type}\nсемейства: {self.__families}\n'
+parrot = Ara('Horde', 'Parrot', 80,'ara', 900,100)
+print(parrot)
+
+
 
 class Amphibia(Reptiles):
     """
@@ -117,20 +143,7 @@ class Reptilia(Reptiles):
 reptilia =  Reptilia('crocodilia', 4, 'crocodylus niloticus', 750, 35)
 print(reptilia)
 
-class Mammals(Animals):
-    """
-    Подкласс описывающий млекопитающих
-    """
-    def __init__(self, body_length: int, name: str, weight: int, mov_speed: int):
-        self.__body_length = body_length
-        super().__init__(name, weight, mov_speed)
-    @property
-    def body_length(self):
-        return self.__body_length
-    def __str__(self):
-        return super().__str__() + f'длинна тела: {self.__body_length}\n'
-mammals = Mammals(3,'kangu', 47, 70)
-print(mammals)
+
 class Marsupialia(Mammals):
     """
     Подкласс описывающий сумчатых
@@ -145,6 +158,8 @@ class Marsupialia(Mammals):
         return super().__str__() + f'виды: {self.__kinds}\n'
 mar = Marsupialia('entomophagy,phytophagy', 3,'kangu', 47, 70)
 print(mar)
+
+
 class Placentalia(Mammals):
     """
     Подкласс описывающий плацентарны
